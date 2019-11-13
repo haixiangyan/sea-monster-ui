40a25974f7f2e017a03c2971a077c6391e9a03d3
Haixiang <haixiang6123@gmail.com>
2019-11-13 12:55:13 -0800

    Move Button component

--- lib/__tests__/Button.unit.tsx
+++ lib/__tests__/Button.unit.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import renderer from 'react-test-renderer'
-import Button from '../Button'
+import Button from '../Button/Button'
 
 describe('Button', () => {
   it('is a div', () => {
