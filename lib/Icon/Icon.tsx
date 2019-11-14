c9bb0e061b1b4167bdfc84304ebc2b7ba7496346
Haixiang <haixiang6123@gmail.com>
2019-11-14 10:27:19 -0800

    Add onClick event handler

--- lib/Icon/Icon.tsx
+++ lib/Icon/Icon.tsx
@@ -4,11 +4,12 @@ import './importIcons'
 
 interface IconProps {
   name?: string
+  onClick?: () => void
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
   return (
-    <svg className="sm-icon">
+    <svg className="sm-icon" onClick={props.onClick}>
       <use xlinkHref={`#${props.name}`}/>
     </svg>
   )
--- lib/index.tsx
+++ lib/index.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom'
 
 import Icon from './Icon/Icon'
 
+const fn = () => {
+  console.log('fn')
+}
+
 ReactDOM.render(
   <div>
-    <Icon name="close"/>
+    <Icon onClick={fn} name="close"/>
     <Icon name="down"/>
   </div> ,
   document.querySelector('#root')
